0849d7cbb75bb1cc88e0237b31d3003393d43e5e
kaivonafsari <kaivon.afsari@gmail.com>
2015-07-08 12:45:41 -0700

    clean-up

--- client/js/models/ResultsEntry.js
+++ client/js/models/ResultsEntry.js
@@ -6,16 +6,6 @@ var ResultsEntry = Backbone.Model.extend({
     monthlyHazardInsurance: "",
     monthlyHoaDues: "",
     monthlyIncome: "",
-    // monthlyIncomeTax: "",
-    // monthlyPmi: "",
-    // monthlyPrincipalAndInterest: "",
-    // monthlyPropertyTaxes: "",
-    // monthlyRemainingBudget: "",
-    // totalInterestPayments: "",
-    // totalMonthlyPayment: "",
-    // totalPayments: "",
-    // totalPrincipal: "",
-    // totalTaxesFeesAndInsurance: ""
   },
 
   url: '/getInfo',
@@ -40,29 +30,6 @@ var ResultsEntry = Backbone.Model.extend({
   getInfo: function(info) {
     console.log("I'm inside the API call!" + JSON.stringify(info));
     this.fetch({data: info, type: 'POST'}).then(this.hailMary.bind(this));
-
-
-    // var that =this;
-    // console.log("IS THIS A MODEL?", this);
-    // $.ajax({
-    //          url: "/stuff",
-    //          headers: { 'Access-Control-Allow-Origin': '*' },
-    //          type: 'POST',
-    //          data: info,
-    //          crossDomain: true,
-    //          dataType: 'json',
-    //          // contentType: 'application/json',
-    //          success: function(data) { 
-    //           defaults = data.response.affordabilityAmount;
-    //           console.log("THIS IS DEFAULTS "+defaults);
-    //           that.trigger("info", that);
-              
-    //         },
-    //         error: function(eroor) { 
-    //           console.log("THIS IS BAD"+ JSON.stringify(eroor));
-    //           // alert('Success!'+data); 
-    //         }
-    //       });
   },
 
   hailMary: function() {
